a3a74737ec8a125740939cc4d99159b652ba34fe
haghl <wdg980813@gmail.com>
2025-05-23 18:04:01 +0900

    Feat: matter.js 물리엔진 용 라이브러리 추가

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,24 @@
-// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {mergeConfig, getDefaultConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    assetExts: ['png', 'jpg', 'jpeg', 'gif', 'webp'],
+    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx'],
+  },
+};
 
-const {
-  wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
-
-const config = {};
-
-module.exports = wrapWithReanimatedMetroConfig(config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
